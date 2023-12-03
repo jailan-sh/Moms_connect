f021ee363aecea26f877dbb43bdb43775ace15e4
jailan-sh <jilan.elsherbiny@gmail.com>
2023-12-04 01:20:09 +0200

    edit authontication of users rrgistrtion

--- Mommyblog/users/views.py
+++ Mommyblog/users/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(request):
-    """ users registration form """
+    """ users registration form and validation """
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
+            messages.success(request, "Your Account {} has been created. ready to Sign In!".format(username))
+            return redirect('blog-home')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
