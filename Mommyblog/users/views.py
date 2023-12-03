dae89f6f96f72ea806c3a2da570d885b8e27ac48
jailan-sh <jilan.elsherbiny@gmail.com>
2023-12-03 23:23:46 +0200

    edit urls of users

--- Mommyblog/users/views.py
+++ Mommyblog/users/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
 def register(request):
-    return render(request, 'users/register.html')
+    """ users registration form """
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
