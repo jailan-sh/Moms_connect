48fad2989c8557112dba58080c08225d1dd96070
jailan-sh <jilan.elsherbiny@gmail.com>
2023-12-06 16:00:28 +0200

    fix bug

--- Mommyblog/users/models.py
+++ Mommyblog/users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     """ profile of user"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.CharField(max_length=1000, blank=True)
+    #Bio = models.CharField(max_length=1000, blank=True)
     image = models.ImageField(default='default.jpg', upload_to='profile_photos')
 
     def __str__(self):
