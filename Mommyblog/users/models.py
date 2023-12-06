094054615e3fddc7589c03d0b5fff3fc8cbb6fa6
jailan-sh <jilan.elsherbiny@gmail.com>
2023-12-06 14:49:45 +0200

    profile edit

--- Mommyblog/users/models.py
+++ Mommyblog/users/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    """ profile of user"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=1000, blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_photos')
+
+    def __str__(self):
+        """return string about username profile"""
+        return '{} profile'.format(self.user.username)
