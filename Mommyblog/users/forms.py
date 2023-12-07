4cb7885633aa53f10df268de36ce7c392512cb0c
jailan-sh <jilan.elsherbiny@gmail.com>
2023-12-07 20:31:12 +0200

    edit pic in home page

--- Mommyblog/users/forms.py
+++ Mommyblog/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -11,3 +12,20 @@ class UserRegisterForm(UserCreationForm):
         """model to interact"""
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    """ update user info."""
+    email = forms.EmailField()
+
+    class Meta:
+        """ model to update"""
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    """ to upsate profile information"""
+    Bio = forms.CharField(max_length=1000, required=False)
+    class Meta:
+        """MODEL TO UPDATE"""
+        model = Profile
+        fields = ['image', 'Bio']
--- Mommyblog/users/models.py
+++ Mommyblog/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
 
 class Profile(models.Model):
     """ profile of user"""
@@ -11,3 +11,13 @@ class Profile(models.Model):
     def __str__(self):
         """return string about username profile"""
         return '{} profile'.format(self.user.username)
+
+    def save(self):
+        """edit some fatures before saving instancd """
+        super().save()
+
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
