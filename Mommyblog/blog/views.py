6c8bc38c9d06233988f1b0903db2fddccdc2e6f7
jailan-sh <jilan.elsherbiny@gmail.com>
2023-12-09 14:54:52 +0200

    add functionality to posts for users

--- Mommyblog/blog/views.py
+++ Mommyblog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -21,8 +22,8 @@ class PostDetailView(DetailView):
     """class to present every post alone"""
     model = Post
 
-class PostCreateView(CreateView):
-    """class to create post"""
+class PostCreateView(LoginRequiredMixin, CreateView):
+    """class to create post by authenticated users"""
     model = Post
     fields = ['title', 'content']
 
@@ -30,6 +31,32 @@ class PostCreateView(CreateView):
         """set author of post"""
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """class to update post"""
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        """set author of post"""
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        """to allow only owner of post can update it """
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """class to delete post"""
+    model = Post
+
+    def test_func(self):
+        """to allow only owner of post change it """
+        post = self.get_object()
+        return self.request.user == post.author
+
+
 
 def about(request):
     """about page for blog"""
